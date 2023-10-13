7af034dc023385c990bab79a8d7aee36dff47d2e
Edwin <edwinob36@gmail.com>
2023-10-13 13:53:17 +0200

    tercer commit

--- src/org/ulpgc/is1/control/Main.java
+++ src/org/ulpgc/is1/control/Main.java
@@ -8,14 +8,33 @@ import org.ulpgc.is1.model.*;
 //Falta hacerlo como lo pide en la practica
 public class Main {
     private static void init(RepairManager workshop) {
-        workshop.addMechanic("Pedro", "Palomo", Repair(Date("05-12-2021"), "Breaks", 12), BreakdownTypes.electrical);
+        ///Primer mecanico
+        Repair repair1 = new Repair(Date "08-04-20022", "cambio aceite", 25 );
+        Mechanic mechanic1 = new Mechamic("Pepe", "Miranda", repair1);
+
+        ///segundo mecanico
+        Repair repair2 = new Repair(Date "07-04-20022", "cambio de neumatico", 30 );
+        Mechanic mechanic2 = new Mechamic("Pedro", "Marrero", repair2);
+
+        ///Primer vehiculo
+        Costumer Costumer1 = new Costumer("Mario", new Phone("345678901"))
+        Vehicle vehicle = new Vehicle("Glass", "Hilux", new Plate("GC20045"));
+
+        ///Piezas en el almacen
+        SparePart part1 = new SparePart("Bumper", 200, new Item(1));
+        SparePart part2 = new SparePart("Tire", 100, new Item(1));
+
+        workshop.addMechanic(mechanic1, repair1, BreakdownTypes.electrical);
         workshop.addVehicle("Oil change", "Hilux", new Plate("GC20020") ,new Costumer("Juan", "630234520"),new Phone("630234520");
-        workshop.repair("Oil", 20);
+        workshop.repair()
     }
     public static void main(String[] args) {
         RepairManager workshop = new RepairManager();
         init(workshop);
-        System.out.printf("El workshop tiene: " + workshop.getMechanic(),   );
+        System.out.printf("Carcteristicas del primer mecanico:   nombre: " + workshop.getMechanic(0).getName() + " Apellido: " + workshop.getMechanic(0).getSurname());
+        System.out.printf("Carcteristicas del segundo vehiculo:   make: " + workshop.getVehicle(1).getMake() + " Model: " + workshop.getVehicle(1).getModel() +" Plate: "+ workshop.getVehicle(1).getPlate.getNumber() + " Costumer name: " + workshop.getVehicle().getCostumer().getName()+" Costumer phone: " + workshop.getVehicle().getCostumer().getPhone());
+
+
 
 
 
--- src/org/ulpgc/is1/model/Item.java
+++ src/org/ulpgc/is1/model/Item.java
@@ -2,7 +2,6 @@ package org.ulpgc.is1.model;
 
 public class Item {
     SparePart Sparepart;
-    Repair repair;
     int quantity;
 
 }
