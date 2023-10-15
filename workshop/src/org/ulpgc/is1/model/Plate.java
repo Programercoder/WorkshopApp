92d56e7c816978a2f3ab4a396a2decf309a60a97
mariyafail <mariafail2304@gmail.com>
2023-10-15 17:52:00 +0100

    Cree un metodo llamado "isValid()" que comprueba si la placa del automovil esta valida

---
+++ workshop/src/org/ulpgc/is1/model/Plate.java
@@ -0,0 +1,25 @@
+package org.ulpgc.is1.model;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Plate {
+    private String number;
+
+    public Plate(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public boolean isValid(String number) {
+        String valid = "^[A-Z]{1} \\d{2} [A-Z]{3}$";
+        Pattern pattern = Pattern.compile(valid);
+        Matcher matcher = pattern.matcher(number);
+        return matcher.matches();
+    }
+}
