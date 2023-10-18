ebfed46b32417bb8b7f0bf4d481e19bb998a1187
Programercoder <80003897+Programercoder@users.noreply.github.com>
2023-10-18 17:44:51 +0200

    Update Item.java
    Finalizé los metodos de la clase Item

--- src/org/ulpgc/is1/model/Item.java
+++ src/org/ulpgc/is1/model/Item.java
@@ -2,6 +2,24 @@ package org.ulpgc.is1.model;
 
 public class Item {
     SparePart Sparepart;
+    Repair repair;
     int quantity;
 
+    public Item(SparePart Sparepart, Repair repair, int quantity){
+        this.Sparepart = Sparepart;
+        this.repair = repair;
+        this.quantity = quantity;
+    }
+
+    public Repair getRepair(){
+        return this.repair;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public SparePart getSparepart() {
+        return Sparepart;
+    }
 }
