ac3c332c5605ade4530559d144f4a9547463a737
Edwin <edwinob36@gmail.com>
2023-10-19 11:11:31 +0200

    Metodos de la clase SparePart getters y setters

--- src/org/ulpgc/is1/model/SparePart.java
+++ src/org/ulpgc/is1/model/SparePart.java
@@ -1,5 +1,31 @@
 package org.ulpgc.is1.model;
-
+import java.util.*;
+###Subir a git este archivo
 public class SparePart {
+    private String name;
+    private int price;
+    private List<Item> item;
 
+    public SaperePart(String name, int price, Item item){
+        this.name = name;
+        this.price = price;
+        this.item = new ArrayList<Item>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public List<Item> getItems() {
+        return item;
+    }
+
+    public addItem(Repair repair, int quantity){
+        Item = new Item(this, reapir, quantity);
+        item.add(Item);
+    }
 }
