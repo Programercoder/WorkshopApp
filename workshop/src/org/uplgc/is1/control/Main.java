7769f8075e2706e0a148ec43efdd685833e7e510
mariyafail <mariafail2304@gmail.com>
2023-10-20 13:00:06 +0100

    "Main" para comprobar el funcionamiento

--- workshop/src/org/uplgc/is1/control/Main.java
+++ workshop/src/org/uplgc/is1/control/Main.java
@@ -9,22 +9,21 @@ public class Main {
     public static void main(String[] args) {
         RepairManager repairManager = new RepairManager();
 
-        // Inyectar información al modelo mediante el método init
         init(repairManager);
-        System.out.println(((Mechanic)repairManager.getMechanic().get(0).getMechanic()));
-        System.out.println(((Vehicle)repairManager.getVehicle().get(1).getVehicle()));
+        System.out.println(((Mechanic)repairManager.getMechanics().get(0)));
+        System.out.println(((Vehicle)repairManager.getVehicles().get(1)));
     }
 
     public static void init(RepairManager repairManager) {
         Mechanic mechanic1 = new Mechanic("Ana", "Perez");
         Mechanic mechanic2 = new Mechanic("David", "Lumusa");
-        Customer customer1 = new Customer("Maria", "643273589");
-        Customer customer2 = new Customer("Diego", "649034678");
-        Vehicle vehicle1 = new Vehicle("Toyota", "Corolla", "A3456CD");
-        Vehicle vehicle2 = new Vehicle("Nissan", "Juke", "F2345SA");
+        Customer customer1 = new Customer("Maria", new Phone("643273589"));
+        Customer customer2 = new Customer("Diego", new Phone("649034678"));
+        Vehicle vehicle1 = new Vehicle("Toyota", "Corolla", new Plate("A3456CD"));
+        Vehicle vehicle2 = new Vehicle("Nissan", "Juke", new Plate("F2345SA"));
         List<Mechanic> mechanics = new ArrayList<>();
         mechanics.add(mechanic1);
-        new Repair(345, "Maletero", "65", mechanics, vehicle2);
+        new Repair(BreakdownTypes.electrical, 365, 65, mechanics, vehicle2);
         repairManager.addVehicle(vehicle1);
         repairManager.addVehicle(vehicle2);
         repairManager.addMechanic(mechanic1);
