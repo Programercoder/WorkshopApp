61f94f7918207ee7e65646b31ab3b6950cc12a96
Programercoder <80003897+Programercoder@users.noreply.github.com>
2023-10-24 12:01:00 +0200

    Update Vehicle.java

--- src/org/ulpgc/is1/model/Vehicle.java
+++ src/org/ulpgc/is1/model/Vehicle.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Vehicle {
 
-
+    ///Vehicle atributes
     private String make;
     private String model;
     private Plate plate;
     private Customer owner;
     private List<Repair> repair;
 
-
+    ///Vehicle constructor
     public Vehicle(String make, String model, Plate plate, Customer customer){
         this.make = make;
         this.model = model;
@@ -27,40 +27,52 @@ public class Vehicle {
         repair.remove(index);
     }
 
+    ///getMake method
     public String getMake() {
         return make;
     }
-
+    
+    ///setMake method
     public void setMake(String make) {
         this.make = make;
     }
-
+    
+    ///getModel method
     public String getModel() {
         return model;
     }
 
+    ///setModel method
     public void setModel(String model) {
         this.model = model;
     }
 
+    ///getPlate method
     public Plate getPlate() {
         return plate;
     }
 
+    ///setPlate method
     public void setPlate(Plate plate) {
         this.plate = plate;
     }
 
-    public Customer getCostumer() {
-        return this.owner;
-    }
 
 
+    ///getRepair method
     public List<Repair> getRepair() {
         return repair;
     }
-
+    
+    ///getOwner method
     public Customer getOwner() {
         return owner;
     }
+
+    ///setOwner method
+    public setOuner(Customer name){
+        this.owner = name; 
+    }
+        
+    
 }
