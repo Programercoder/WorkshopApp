fb73d2a37946402935ab85de5bb823fdaffcf68f
Programercoder <80003897+Programercoder@users.noreply.github.com>
2023-10-24 12:07:02 +0200

    Update Phone.java

--- src/org/ulpgc/is1/model/Phone.java
+++ src/org/ulpgc/is1/model/Phone.java
@@ -4,21 +4,29 @@ import java.util.regex.Matcher;
 
 ///Preguntar sobre clases primitivas
 public class Phone {
+
+    ///Atributes
     private String number;
 
+    ///Phone constructor
     public Phone(String number){
-        this.number = number;
+        if isValid(number) {    
+            this.number = number;
+        }
+        else {
+            this.number = "XXXX";
     }
-    //getter de number
+        
+    //getter of number
     public String getNumber() {
         return number;
     }
-    //Setter de number
+    //Setter of number
     public void setNumber(String number) {
         this.number = number;
     }
 
-    //Metodo isValid
+    //isValid method
     public boolean isValid(String number) {
         String valid = "\\d{9}";
         Pattern pattern = Pattern.compile(valid);
@@ -26,7 +34,6 @@ public class Phone {
         if (matcher.matches()) {
             return true;
         } else {
-            this.number = "XXXX";
             return false;
         }
     }
