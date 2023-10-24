a4d0a3fdbc3bf6428d2ad2d65f378587fd510e0c
Programercoder <80003897+Programercoder@users.noreply.github.com>
2023-10-24 13:10:26 +0200

    Update Mechanic.java

--- src/org/ulpgc/is1/model/Mechanic.java
+++ src/org/ulpgc/is1/model/Mechanic.java
@@ -3,32 +3,40 @@ import java.util.*;
 
 
 public class Mechanic {
+
+    ///Atributes
     private String name;
     private String surname;
     private List<Repair> repair;
 
+    ///Constructor
     public Mechanic(String name, String surname){
         this.name = name;
         this.surname = surname;
         this.repair = new ArrayList<Repair>();
     }
 
+    ///getSurname method
     public String getSurname() {
         return surname;
     }
 
+    ///getName method
     public String getName() {
         return name;
     }
 
+    ///getRepair method
     public List<Repair> getRepair(int index) {
         return repair;
     }
 
+    ///addRepair method
     public void addRepair(Repair reparation){
         repair.add(reparation);
     }
 
+    ///deleteRepair method
     public void deleteRepair(int index) {
         if (index < 0 || index >= repair.size()) {
             throw new IndexOutOfBoundsException("Índice fuera de rango");
