3903a5f9e44324b466ae4a422f337fa432a6d9d6
Programercoder <80003897+Programercoder@users.noreply.github.com>
2023-10-24 13:12:11 +0200

    Update Payment.java

--- src/org/ulpgc/is1/model/Payment.java
+++ src/org/ulpgc/is1/model/Payment.java
@@ -6,28 +6,34 @@ import java.text.SimpleDateFormat;
 public class Payment {
 
 
+    ///Atributes
     private Date date;
     private int ammount;
 
+    ///Constructor
     public Payment(Date date, int ammount) {
         this.date = date;
         this.ammount = ammount;
     }
 
+    ///getDate method
     public String getDate() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy MM dd");
         String formated = dateFormat.format(this.date);
         return formated;
     }
 
+    ///setDate method
     public void setDate(Date date) {
         this.date = date;
     }
 
+    ///getAmmount method
     public int getAmmount() {
         return ammount;
     }
 
+    ///setAmmount method
     public void setAmmount(int ammount) {
         this.ammount = ammount;
     }
